c3e873c66e882ff5a5d65805587fdcc4b3aad065
Rahul Badade <rahulvbadade@gmail.com>
2022-12-24 14:43:22 +0530

    fix: added cors

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const cors = require('cors');
 
 server.use(middlewares)
+server.use(cors());
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
